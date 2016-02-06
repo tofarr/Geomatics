4dfe63437427a5e40700deba472ec954ae46d7ea
Tim O'Farrell <tofarrell@espatial.com>
2016-02-06 23:09:54 +0000

    Boosted test coverage on LineString

--- JGeomatics/src/org/jg/LineString.java
+++ JGeomatics/src/org/jg/LineString.java
@@ -17,29 +17,7 @@ import java.util.Collection;
 public final class LineString implements Externalizable, Cloneable {
 
     private final VectList vects;
-    private RTree<Line> lines;
-            /*
-            ABOUT BUILDERS?
-            Would have to write:
-            VectBuilder
-            LineBuilder
-            RectBuilder
-            
-            VectList/Set/Map remain as is.
-            Map and Tree cant be immutable - they reference others
-                
-            LineString
-            Ring
-            RingSet become immutable
-                    
-            but can be invalid.
-                    
-                    THINK ABOUT THIS.
-            TO MAKE IMMUTABLE - NEVER RETURN LINES - SIMPLY USE ITS GET METHODS
-    
-            So maybe we use builder pattern for primitives only?
-            Or maybe we make primitives immutable
-            */
+    private RTree<Integer> lineIndex;
 
     @ConstructorProperties({"vects"})
     public LineString(VectList vects) {
@@ -49,25 +27,14 @@ public final class LineString implements Externalizable, Cloneable {
         this.vects = vects.clone();
     }
 
-    public LineString() {
-        this(new VectList());
+    LineString(VectList vects, RTree<Integer> lineIndex) {
+        this.vects = vects;
+        this.lineIndex = lineIndex;
     }
 
-    public boolean isValid() {
-        return (vects.size() > 1);
-    }
-
-    public VectList getVects(VectList target) {
-        return target.addAll(vects);
-    }
-
-    public int size() {
-        return vects.size() - 1;
-    }
-
-    public void normalize(Tolerance tolerance) {
-        if (vects.isEmpty()) {
-            return;
+    public boolean isValid(Tolerance tolerance) {
+        if (vects.size() < 2) {
+            return false;
         }
 
         //Remove any duplicate points
@@ -77,36 +44,118 @@ public final class LineString implements Externalizable, Cloneable {
             double bx = vects.getX(b);
             double by = vects.getY(b);
             if (tolerance.match(ax, ay, bx, by)) {
-                vects.remove(b);
+                return false;
             }
         }
 
         //Remove any colinear points
         double toleranceSq = tolerance.getTolerance();
         toleranceSq *= toleranceSq;
-        for (int c = vects.size() - 1, b = vects.size() - 1, a = b - 1; a >= 0; a--, b--) {
+        for (int c = vects.size() - 1, b = c - 1, a = b - 1; a >= 0; a--, b--, c--) {
             double ax = vects.getX(a);
             double ay = vects.getY(a);
             double bx = vects.getX(b);
             double by = vects.getY(b);
             double cx = vects.getX(c);
             double cy = vects.getY(c);
-            if (Line.pntSegDistSq(ax, ay, bx, by, cx, cy) <= toleranceSq) {
-                vects.remove(b);
+            if (Line.pntSegDistSq(ax, ay, cx, cy, bx, by) <= toleranceSq) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public VectList getVects(VectList target) {
+        return target.addAll(vects);
+    }
+
+    public Vect get(int index, Vect target) {
+        return vects.get(index, target);
+    }
+
+    public Line get(int index, Line target) {
+        return vects.get(index, target);
+    }
+
+    public boolean getInteractingLines(Rect bounds, final Processor<Line> lineProcessor, final Line target) {
+        return getLineIndex().getInteracting(bounds, new NodeProcessor<Integer>() {
+            @Override
+            public boolean process(SpatialNode<Integer> leaf, int index) {
+                index = leaf.getItemValue(index);
+                vects.get(index, target);
+                return lineProcessor.process(target);
+            }
+
+        });
+    }
+
+    public boolean getOverlappingLines(Rect bounds, final Processor<Line> lineProcessor, final Line target) {
+        return getLineIndex().getOverlapping(bounds, new NodeProcessor<Integer>() {
+            @Override
+            public boolean process(SpatialNode<Integer> leaf, int index) {
+                index = leaf.getItemValue(index);
+                vects.get(index, target);
+                return lineProcessor.process(target);
+            }
+
+        });
+    }
+
+    public int size() {
+        return Math.max(vects.size() - 1, 0);
+    }
+
+    public LineString normalize(Tolerance tolerance) {
+        if (vects.isEmpty()) {
+            return this;
+        }
+
+        VectList _vects = vects.clone();
+        boolean changed = false;
+
+        //Remove any duplicate points
+        for (int b = _vects.size() - 1, a = b - 1; a >= 0; a--, b--) {
+            double ax = _vects.getX(a);
+            double ay = _vects.getY(a);
+            double bx = _vects.getX(b);
+            double by = _vects.getY(b);
+            if (tolerance.match(ax, ay, bx, by)) {
+                _vects.remove(b);
+                changed = true;
+            }
+        }
+
+        //Remove any colinear points
+        double toleranceSq = tolerance.getTolerance();
+        toleranceSq *= toleranceSq;
+        for (int c = _vects.size() - 1, b = c - 1, a = b - 1; a >= 0; a--, b--, c--) {
+            double ax = _vects.getX(a);
+            double ay = _vects.getY(a);
+            double bx = _vects.getX(b);
+            double by = _vects.getY(b);
+            double cx = _vects.getX(c);
+            double cy = _vects.getY(c);
+            if (Line.pntSegDistSq(ax, ay, cx, cy, bx, by) <= toleranceSq) {
+                _vects.remove(b);
+                changed = true;
             }
         }
 
         //reverse if required
         int c = 0;
         int min = 0;
-        int max = size() - 1;
-        while ((min < max) && ((c = Vect.compare(vects.getX(min), vects.getY(min), vects.getX(max), vects.getY(max))) == 0)) {
+        int max = _vects.size() - 1;
+        while ((min < max) && ((c = Vect.compare(_vects.getX(min), _vects.getY(min), _vects.getX(max), _vects.getY(max))) == 0)) {
             min++;
             max--;
         }
         if (c > 0) {
-            vects.reverse();
+            _vects.reverse();
+            changed = true;
         }
+        
+        return changed ? new LineString(_vects, null) : this;
     }
 
     public void splitOnSelfIntersect(Tolerance tolerance, Collection<LineString> results) {
@@ -117,25 +166,35 @@ public final class LineString implements Externalizable, Cloneable {
         network.extractLines(results);
     }
 
-    public RTree<Line> getLines() {
+    RTree<Integer> getLineIndex() {
+        RTree<Integer> ret = lineIndex;
+        if (ret == null) {
+            ret = calculateLineIndex(vects);
+            lineIndex = ret;
+        }
+        return ret;
+    }
+
+    static RTree<Integer> calculateLineIndex(VectList vects) {
         if (vects.size() <= 1) {
-            return new RTree<>(new Rect[0], new Line[0]);
+            return new RTree<>(new Rect[0], new Integer[0]);
         }
         double[] bounds = new double[(vects.size() - 1) << 2];
-        Line[] lines = new Line[vects.size()-1];
+        Integer[] indices = new Integer[vects.size() - 1];
         Rect rect = new Rect();
-        for(int i = 1, j = 0; i < vects.size(); i++){
-            int k = i-1;
-            Line line = new Line(vects.getX(k), vects.getY(k), vects.getX(i), vects.getY(i));
+        Line line = new Line();
+        for (int i = 1, j = 0; i < vects.size(); i++) {
+            int k = i - 1;
+            line.set(vects.getX(k), vects.getY(k), vects.getX(i), vects.getY(i));
             line.normalize();
             line.getBounds(rect);
             bounds[j++] = rect.getMinX();
             bounds[j++] = rect.getMinY();
             bounds[j++] = rect.getMaxX();
             bounds[j++] = rect.getMaxY();
-            lines[k] = line;
+            indices[k] = k;
         }
-        RTree<Line> tree = new RTree<>(bounds, lines);
+        RTree<Integer> tree = new RTree<>(bounds, indices);
         return tree;
     }
 
@@ -144,9 +203,9 @@ public final class LineString implements Externalizable, Cloneable {
         if (size == 0) {
             return;
         }
-        RTree<Line> lines = other.getLines();
+        RTree<Integer> lines = other.getLineIndex();
         VectList intersections = new VectList();
-        SplitAgainstProcessor processor = new SplitAgainstProcessor(lines, tolerance, intersections);
+        SplitAgainstProcessor processor = new SplitAgainstProcessor(other.vects, tolerance, intersections);
         VectList result = new VectList();
         size--;
         Rect rect = new Rect();
@@ -155,10 +214,10 @@ public final class LineString implements Externalizable, Cloneable {
         for (int i = 0; i < size; i++) {
             addNonMatching(result, i);
             processor.reset(i, vects);
-            processor.line.getBounds(rect);
+            processor.a.getBounds(rect);
             lines.getRoot().getInteracting(rect, processor);
             if (processor.intersections.size() > 0) {
-                sortByDist(processor.line.ax, processor.line.ay, intersections, a, b);
+                sortByDist(processor.a.ax, processor.a.ay, intersections, a, b);
                 for (int n = 0; n < intersections.size(); n++) {
                     addNonMatching(result, i);
                     if (result.size() > 1) {
@@ -271,17 +330,19 @@ public final class LineString implements Externalizable, Cloneable {
         vects.readExternal(in);
     }
 
-    static class SplitAgainstProcessor implements NodeProcessor<Line> {
+    static class SplitAgainstProcessor implements NodeProcessor<Integer> {
 
-        final RTree<Line> lines;
-        final Line line;
+        final VectList vects;
+        final Line a;
+        final Line b;
         final Vect intersection;
         final VectList intersections;
         final Tolerance tolerance;
 
-        SplitAgainstProcessor(RTree<Line> lines, Tolerance tolerance, VectList intersections) {
-            this.lines = lines;
-            this.line = new Line();
+        SplitAgainstProcessor(VectList vects, Tolerance tolerance, VectList intersections) {
+            this.vects = vects;
+            this.a = new Line();
+            this.b = new Line();
             this.intersection = new Vect();
             this.intersections = intersections;
             this.tolerance = tolerance;
@@ -289,13 +350,14 @@ public final class LineString implements Externalizable, Cloneable {
 
         void reset(int index, VectList vects) {
             intersections.clear();
-            vects.get(index, line);
+            vects.get(index, a);
         }
 
         @Override
-        public boolean process(SpatialNode<Line> leaf, int index) {
-            Line value = leaf.getItemValue(index);
-            if (line.intersectionSeg(line, tolerance, intersection)) {
+        public boolean process(SpatialNode<Integer> leaf, int index) {
+            index = leaf.getItemValue(index);
+            vects.get(index, b);
+            if (a.intersectionSeg(b, tolerance, intersection)) {
                 intersections.add(intersection);
             }
             return true;
